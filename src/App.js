d3208af71213ab8b2740ca64ad06a7e30417b1bb
kyo <kwonkyo@gmail.com>
2022-01-18 21:08:43 +0000

    button, css 모듈 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+ import Button from './Button';
+ import styles from './App.module.css';
+ import {useState} from 'react';
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev+1 );
   return (
     <div>
-      <h1>Hello!!</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button 
+        onClick={onClick} 
+        text={"Click me"} />
     </div>
   );
 }
