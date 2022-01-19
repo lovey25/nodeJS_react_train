42afb3bf8870ae948a86b0673a3a9208d2d3f055
kwonkyo <kwonkyo@gmail.com>
2022-01-19 13:57:06 +0900

    Coin 조회 앱

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
  import Button from './Button';
  import styles from './App.module.css';
- import {useState} from 'react';
+ import {useState, useEffect} from 'react';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue(prev => prev+1 );
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button 
-        onClick={onClick} 
-        text={"Click me"} />
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? 
+        <strong>Loading...</strong> : 
+        <select>
+        {coins.map((coin) => (
+          <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</option>
+        ))}
+        </select>  
+      }
+      
     </div>
   );
 }
